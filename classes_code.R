83a51ca9fca489dd2b0a20ca727ac1586ccf0c09
Laura Acion <17729900+lauracion@users.noreply.github.com>
2018-07-10 19:12:41 -0300

    adds last changes

--- classes_code.R
+++ classes_code.R
@@ -2,3 +2,11 @@
 install.packages(c("ggplot2", "plyr", "gapminder"))
 
 # Project Management With RStudio
+
+# Seeking Help
+
+# Data Structures
+cats <- read.csv(file = "data/feline-data.csv")
+cats
+
+# Exploring Data Frames

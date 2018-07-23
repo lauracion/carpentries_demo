8a92eefb333e7283ed8ad4be7f25a96342b3df43
Laura Acion <17729900+lauracion@users.noreply.github.com>
2018-07-23 14:34:07 -0300

    done reading for demo

--- classes_code.R
+++ classes_code.R
@@ -1,5 +1,11 @@
 # Introduction to R and RStudio
 install.packages(c("ggplot2", "plyr", "gapminder"))
+install.packages(c("tidyr","dplyr"))
+library(ggplot2)
+library(plyr)
+library(gapminder)
+library(tidyr)
+library(dplyr)
 
 # Project Management With RStudio
 
@@ -13,3 +19,25 @@ cats
 gapminder <- read.csv("data/gapminder-FiveYearData.csv")
 
 # Subsetting Data
+
+# Control Flow
+
+# Creating Publication-Quality Graphics
+
+# Vectorization
+
+# Functions Explained
+source("functions/functions-lesson.R")
+head(calcGDP(gapminder, year = 2007))
+
+# Writing Data
+
+# Split-Apply-Combine
+
+# Dataframe Manipulation with dplyr
+
+# Dataframe Manipulation with tidyr
+
+# Producing Reports With knitr
+
+# Writing Good Software
